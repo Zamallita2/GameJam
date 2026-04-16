6591a4c8941acaf760259adb40a64e5ab2580284
Carla Andrea Camacho Sanizo <143283468+Carlaandrea22@users.noreply.github.com>
2026-04-16 11:51:40 -0400

    Primer commit
    Player_Movement

---
+++ Assets/01_Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float walkSpeed = 3f;
+    public float runSpeed = 6f;
+
+    public CharacterController controller;
+    public Animator animator;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(h, 0, v);
+
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        float currentSpeed = isRunning ? runSpeed : walkSpeed;
+
+        controller.Move(move * currentSpeed * Time.deltaTime);
+
+        // 🔥 ESTA ES LA PARTE IMPORTANTE
+        float movementAmount = move.magnitude;
+
+        if (isRunning && movementAmount > 0.1f)
+            animator.SetFloat("Speed", 1f);
+        else if (movementAmount > 0.1f)
+            animator.SetFloat("Speed", 0.5f);
+        else
+            animator.SetFloat("Speed", 0f);
+    }
+}
